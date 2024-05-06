35968d1ea459e61bfa731f982fdaebdde2826d2e
System administrator <EmikoJenn>
2024-05-06 12:55:28 -0600

    Hot Backup

---
+++ mountpoints.nix
@@ -0,0 +1,22 @@
+{ config, lib, pkgs, modulesPath, ... }: {
+  fileSystems = {
+    "/run/media/emikojenn/Respaldo" = {
+      device = "dev/disk/by-uuid/8472BBDC72BBD0E2";
+      fsType = "ntfs";
+      options = [
+        "users"
+	"nofail"
+	"x-gvfs-show"
+      ];
+    };
+    "/run/media/emikojenn/my_life" = {
+      device = "/dev/disk/by-uuid/c0c0f981-2950-4a5e-96a5-57c4637d1357";
+      fsType = "btrfs";
+      options = [
+        "nofail"
+	"x-gvfs-show"
+      ];
+    };
+  };
+}
+
