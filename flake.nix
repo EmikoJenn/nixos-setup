64a4371778e18e2aae839b798f00d46d39b21ba0
System administrator <EmikoJenn>
2024-05-07 12:30:50 -0600

    emiko setup stable v1

--- flake.nix
+++ flake.nix
@@ -2,14 +2,25 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, nixpkgs-wayland, hyprland, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs.legacyPackages.${system}  {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in
+  {
+    nixosConfigurations.emikojenn = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+      ];
+    };
   };
 }
