944594f2006918e9ca1d6f2ffe82f4a5d1e9b562
EmikoJenn <EmikoJenn>
2024-05-10 14:29:30 -0600

    Steam WIP

--- modules/home-manager/emikojenn/default.nix
+++ modules/home-manager/emikojenn/default.nix
@@ -8,6 +8,7 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     ./programs
+    ./packages
     #./containers
     #./theme/gtk.nix
   ];
--- modules/home-manager/emikojenn/programs/default.nix
+++ modules/home-manager/emikojenn/programs/default.nix
@@ -4,7 +4,6 @@
     home-manager.enable = true;
     git = import ./git.nix;
     nixvim = import ./nixvim.nix { inherit pkgs; };
-    steam = import ./steam;
     btop.enable = true;
     yt-dlp.enable = true;
   };
