8a01d6160d6b50a953a7c46fb9ae795e5eb1e2fc
EmikoJenn <EmikoJenn>
2024-05-13 09:13:13 -0600

    Add C++ v2

--- modules/home-manager/emikojenn/packages/default.nix
+++ modules/home-manager/emikojenn/packages/default.nix
@@ -2,25 +2,5 @@
 {
   home.packages = with pkgs; [
     steam
-    neovim
-    SDL2
-    unzip
-    gnumake
-    fd
-    vulkan-tools
-    vulkan-loader
-    glfw
-    glm
-    xorg.libXi
-    xorg.libXxf86vm
-    syncthing
-    clang
-    coreutils
-    ripgrep
-    glib
-    raylib
-    gcc
-    git
-    gdb
   ];
 }
--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -31,6 +31,25 @@
       dive
       vlc
       libvlc
+      tmux
+      SDL2
+      unzip
+      gnumake
+      fd
+      vulkan-tools
+      vulkan-loader
+      glfw
+      glm
+      xorg.libXi
+      xorg.libXxf86vm
+      syncthing
+      clang
+      coreutils
+      ripgrep
+      glib
+      raylib
+      gcc
+      gdb
     ];
   };
 
