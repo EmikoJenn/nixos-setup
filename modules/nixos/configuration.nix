52977d05ef6195b04ed62143392baaf5c99bb05b
EmikoJenn <EmikoJenn>
2024-05-14 22:32:04 -0600

    Clean up and Add Podman

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -17,7 +17,18 @@
     driSupport = true;
     driSupport32Bit = true;
     extraPackages =  with pkgs; [ amdvlk mangohud ];
-    extraPackages32 = with pkgs; [ driversi686Linux.amdvlk mangohud ];
+    extraPackages32 = with pkgs; [ 
+      driversi686Linux.amdvlk
+      mangohud 
+    ];
+  };
+
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      defaultNetwork.settings.dns_enable = true;
+    };
   };
 
   # ENV
@@ -30,6 +41,7 @@
       git
       wget
       dive
+      podman-compose
       vlc
       libvlc
       tmux
@@ -70,17 +82,7 @@
     allowUnfree = true;
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
   time.timeZone = "America/Chihuahua";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
@@ -123,12 +125,6 @@
     proggyfonts
   ];
 
-
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
   security.polkit.enable = true;
 
   programs = {
@@ -159,13 +155,6 @@
     };
   };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;  
-
   users = {
     extraGroups.docker.members = [ "username-with-access-to-socket" ];
     users.emikojenn = {
@@ -195,45 +184,6 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixO>
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your >
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options>
   system = {
     stateVersion = "23.11";
   };
