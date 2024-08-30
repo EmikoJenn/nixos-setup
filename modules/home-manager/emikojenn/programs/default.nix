01fd50405a54ebb6279f308a5a96f603b87caf4b
EmikoJenn <EmikoJenn>
2024-08-30 15:20:29 -0600

    test(neovim)

--- modules/home-manager/emikojenn/programs/default.nix
+++ modules/home-manager/emikojenn/programs/default.nix
@@ -3,7 +3,6 @@
   programs = {
     home-manager.enable = true;
     git = import ./git.nix;
-    nixvim = import ./nixvim.nix { inherit pkgs; };
     #alvr = import ./alvr.nix;
     btop.enable = true;
     yt-dlp.enable = true;
