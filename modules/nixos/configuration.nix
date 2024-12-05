2ccd693084e8d3ad1e7b2ce137da27f84d4939e4
EmikoJenn <TabrisSoreth@gmail.com>
2024-12-05 11:29:51 -0600

    alvr(feat): ALVR setup 8

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -186,8 +186,17 @@
     xfconf.enable = true;
     steam = {
       enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
+      package = pkgs.steam.override {
+        extraPkgs = pkgs: with pkgs; [
+          corefonts
+          vistafonts
+        ];
+      };
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      protontricks.enable = true;
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
     appimage.enable = true;
     fish.enable = true;
