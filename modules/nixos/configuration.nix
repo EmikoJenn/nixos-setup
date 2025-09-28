a4aa408d63aacc0423cb01fbbe27f359b9f1759e
EmikoJenn <TabrisSoreth@gmail.com>
2025-09-27 20:25:38 -0600

    Fix(Overlay): Proton GE RTSP overlay 16

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -216,12 +216,9 @@ in
         proton-ge-bin
         proton-ge-rtsp-bin
       ];
-    };
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-      protontricks.enable = true;
-      extraCompatPackages = with pkgs; [ proton-ge-bin ];
     };
     appimage.enable = true;
     fish.enable = true;
