e1ca34496f4ce446c77d7d1f282ee2bac96f9e19
EmikoJenn <TabrisSoreth@gmail.com>
2025-10-30 09:52:00 -0600

    Fix(dotnet path)

--- modules/home-manager/emikojenn/packages/default.nix
+++ modules/home-manager/emikojenn/packages/default.nix
@@ -5,7 +5,15 @@
     prismlauncher
     thunderbird
     gparted
+    dotnet-sdk_10
   ];
+
+  home.sessionVariables = {
+    DOTNET_ROOT = "${pkgs.dotnet-sdk_10}/share/dotnet";
+    DOTNET_ROOT_X64 = "${pkgs.dotnet-sdk_10}/share/dotnet";
+  };
+
+  home.sessionPath = [ "$HOME/.dotnet/tools" ];
 
   programs.zathura = {
     enable = true;
