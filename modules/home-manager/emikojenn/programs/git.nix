dd855323046d75e9716234b0d55aa76e5282c052
EmikoJenn <TabrisSoreth@gmail.com>
2025-11-01 15:26:21 -0600

    Update Flake

--- modules/home-manager/emikojenn/programs/git.nix
+++ modules/home-manager/emikojenn/programs/git.nix
@@ -1,24 +1,20 @@
 {
-  enable = true;
-  userName = "EmikoJenn";
-  userEmail = "TabrisSoreth@gmail.com";
-  #signing = {
-    #key = "";
-    #signByDefault = true;
-  #};
-  extraConfig = {
-    #pull.rebase = true;
-    init.defaultBranch = "main";
-    #merge = {
-      #tool = "nvim-mergetool";
-      #conflictstyle = "diff3";
-    #};
-    #mergetool.nvim-mergetool = {
-      #cmd = ''
-        #nvim -f -c "MergetoolStart" "$MERGED" "$BASE" "$LOCAL" "$REMOTE"
-      #'';
-      #trustExitCode = true;
-    #};
-    #mergetool.prompt = false;
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    settings = {
+      user.name = "EmikoJenn";
+      user.email = "EmikoJenn@proton.me";
+
+      init.defaultBranch = "main";
+      pull.rebase = true;
+
+      alias = {
+        cfg = "config --list";
+        uncommit = "reset --soft HEAD^";
+        logall = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      };
+    };
   };
 }
