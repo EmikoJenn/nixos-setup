566ab6ecbc1965edfd9820e58591fb0f191260c1
EmikoJenn <TabrisSoreth@gmail.com>
2025-11-01 15:27:17 -0600

    Update Flake

--- modules/home-manager/emikojenn/programs/git.nix
+++ modules/home-manager/emikojenn/programs/git.nix
@@ -1,20 +1,18 @@
 {
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
+  enable = true;
+  lfs.enable = true;
 
-    settings = {
-      user.name = "EmikoJenn";
-      user.email = "EmikoJenn@proton.me";
+  settings = {
+    user.name = "EmikoJenn";
+    user.email = "EmikoJenn@proton.me";
 
-      init.defaultBranch = "main";
-      pull.rebase = true;
+    init.defaultBranch = "main";
+    pull.rebase = true;
 
-      alias = {
-        cfg = "config --list";
-        uncommit = "reset --soft HEAD^";
-        logall = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-      };
+    alias = {
+      cfg = "config --list";
+      uncommit = "reset --soft HEAD^";
+      logall = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
     };
   };
 }
