03c84c254487adf40745065e3f5c1649546613af
EmikoJenn <TabrisSoreth@gmail.com>
2025-11-03 23:55:58 -0600

    slimevr firewall allow

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -343,7 +343,7 @@ in
     firewall = {
       enable = true;
       allowedTCPPorts = [ 80 443 8080 25565];
-      allowedUDPPorts = [ 51820 25565 9943 9944 9945 9946 9947 9948 9949 ];
+      allowedUDPPorts = [ 6969 51820 25565 9943 9944 9945 9946 9947 9948 9949 ];
     };
     nat = {
       enable = true;
