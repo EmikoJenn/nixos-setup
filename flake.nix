f012cf97f0f49d4a5a3f8817e274f5be92e882e3
EmikoJenn <TabrisSoreth@gmail.com>
2025-11-15 16:27:03 -0600

    flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Main Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -30,7 +30,7 @@
       modules = [
 	      ./modules/nixos/configuration.nix
 
-				createHome {
+				home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
