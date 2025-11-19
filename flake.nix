a6d13d4928f01d88ef32da50d2dc64d7d4f649fa
EmikoJenn <TabrisSoreth@gmail.com>
2025-11-19 10:40:43 -0600

    fix gdtoolkit

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable"
+      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
     };
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
     # nixvim = {
