70a41281e8f3891b953a6e6ddeb35f4b281d217e
EmikoJenn <TabrisSoreth@gmail.com>
2025-11-22 18:51:13 -0600

    test unstable packages

--- modules/home-manager/emikojenn/programs/git.nix
+++ modules/home-manager/emikojenn/programs/git.nix
@@ -2,16 +2,12 @@
   enable = true;
   lfs.enable = true;
 
-  settings = {
-    user.name = "EmikoJenn";
-    user.email = "EmikoJenn@proton.me";
-
-    init.defaultBranch = "main";
-    pull.rebase = true;
-
-  };
+  userName = "EmikoJenn";
+  userEmail = "EmikoJenn@proton.me";
 
   extraConfig = {
+    init.defaultBranch = "main";
+    pull.rebase = true;
     alias = {
       cfg = "config --list";
       uncommit = "reset --soft HEAD^";
